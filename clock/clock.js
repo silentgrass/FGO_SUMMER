a7a5a204028ece05092b6d4fe5034b0878ebf5bd
灰草 <sarzty@yahoo.com.tw>
2016-10-05 19:53:17 +0800

    Update clock.js

--- clock/clock.js
+++ clock/clock.js
@@ -1,18 +1,19 @@
 function startTime() {
     var today = new Date();
-	var Y = today.getFullYear();
-	var M = today.getMonth()+1;
-	var D = today.getDate();
+    var Y = today.getFullYear();
+    var M = today.getMonth()+1;
+    var D = today.getDate();
 	
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
 
-
+    M = cheakTime(M);
+    D = cheakTime(D);
     m = checkTime(m);
     s = checkTime(s);
 
-    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
+    document.getElementById('clock').innerHTML = Y+"/"+M+"/"+D+" "+h+":"+m+":"+s;
     setTimeout(function(){startTime()},200);
 }
 
